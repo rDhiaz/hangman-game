48a4352869fb9ad36fe284cd1319fba18091e643
Mardhiatul I. I <74031371+rDhiaz@users.noreply.github.com>
2021-10-25 11:52:31 +0000

    - adding logo and stage image - add two new files - clean up script

--- main.py
+++ main.py
@@ -1,31 +1,47 @@
 import random
+from hangman_art import logo , stages
+from hangman_words import word_list
 
-word_list = ("aardvark,baboon,camel").split(",")
+
 chosen_word = random.choice(word_list)
 lives = 6
 display = []
 
+print(logo)
+
 for n in chosen_word:
     display += "_"
+print (' '.join(display))
 
 end_of_game = False
-
 while not end_of_game:
-    lives = 0
-    guess = (input("Choose a letter: ")).lower()
-    lst_chosenWord = list(chosen_word)
-    for n in range(len(chosen_word)):
-        
-        if guess == lst_chosenWord[n]:
-            display[n] = guess
-        elif "_" not in display:
-            end_of_game = True
-        elif guess not in lst_chosenWord[n]:
-            lives += 1
-       
-        
-    print (display)
-    print (lives)
+    guess = input("Guess a letter: ").lower()
+    chosen_word_lst = list(chosen_word)
 
+    if guess in display:
+        print(f"You already choose {guess}, please guess different letter.")
+
+    for n in range(len(chosen_word_lst)):
+        if guess == chosen_word_lst[n]:
+            display[n] = guess
+    print(' '.join(display))
+
+    if '_' not in display:
+        end_of_game = True
+        print("You win.")
+
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"{guess} is not in the word.")
+        print(stages[lives])
+
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+    
+
+
+
+    
 
 
